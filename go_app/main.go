4d49e952b5ca110e3eb1dee9cef1d6b8c52de9ed
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-03-22 17:26:32 +0000

    go

---
+++ go_app/main.go
@@ -0,0 +1,34 @@
+// Created by: Mohamad
+// Created on: Mar 2023
+//
+// This program finds the area and perimeter of a rectangle
+
+package main
+
+import "fmt"
+
+/* 
+* This function calculates the area of a triangle
+*/
+func main() {
+	// variables for calculation
+	var base int
+	var height int
+	var area int
+
+	// Recieve Base value from user
+	fmt.Println("What is the base of the triangle? ")
+	fmt.Scanln(&base)
+
+	// Recieve Height value from user
+	fmt.Println("What is the height of the triangle? ")
+	fmt.Scanln(&height)
+
+	// Calculate area
+	area = (base * height) / 2
+
+	// Output
+	fmt.Println("The area of the triangle is: ", area, "cm²")
+
+	fmt.Println("Done.")
+}
