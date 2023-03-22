cfb311a8cbee64a4b8e5bd18ffa296b813e261a3
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-03-22 17:03:36 +0000

    built website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Mohamad All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohamad
+// Created on: Sep 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/*
+* This function calculates the area of a triangle
+*/
+function calculate () {
+
+  // Debugging statements
+  console.log("The button was clicked")
+
+  // Assign the user input to a variable
+  const triangleHeight = parseInt(document.getElementById("height-of-triangle").value)
+
+
+  const triangleBase = parseInt(
+    document.getElementById("base-of-triangle").value
+  )
+
+  // Calculate the area of the triangle
+  const area = (triangleBase * triangleHeight) / 2
+
+  // Display the area of the triangle
+  document.getElementById("answer").innerHTML =
+    "The area of the triangle is " + area + " cm²"
+}
